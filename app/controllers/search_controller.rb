db818c52126e71219ebe8155d2416a3fbe0dfdf1
Garrett Bowman <garrettbowman248@gmail.com>
2024-07-19 11:58:05 -0500

    Questions in my controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def index
     @nation = params[:nation]
-    @characters = AvatarFacade.get_characters(@nation)
+    @characters = AvatarFacade.get_characters(@nation)  # Cant decide where the best place would be for making a characters count
+    # was thinking maybe a class method and get this all characters out of the controller
     @first_25_characters = AvatarFacade.get_first_25_characters(@nation)
   end
 end
